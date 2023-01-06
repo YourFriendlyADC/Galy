67baa834f51d1c55dab33f613bc1a987a5dfd9e7
Alejandro Hernández Polo <jhalejandro@uninorte.edu.co>
2023-01-06 12:41:26 -0500

    first commit, protoype

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menuToggle = document.querySelector('.menuToggle');
+    var nav = document.querySelector('.nav');
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+}
